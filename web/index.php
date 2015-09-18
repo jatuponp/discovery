5c555d27951184933c49c501aeb37e98053136e6
jatuponp <jatuponp@gmail.com>
2015-09-18 16:05:39 +0700

    commit images

--- web/index.php
+++ web/index.php
@@ -1,12 +1,23 @@
 <?php
-
+session_start();
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../config/web.php');
+
+$_l = stripslashes($_REQUEST['_lang']);
+if($_l){
+    $_SESSION['_lang'] = $_l;
+}
+$_lang = $_SESSION['_lang'];
+if($_lang){
+    $config['language'] = $_lang;
+}else{
+    $config['language'] = 'th_TH';
+}
 
 (new yii\web\Application($config))->run();
