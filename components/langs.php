4d687cbd6da35c5471773e3d62d2e4e3af53177d
jatuponp <jatuponp@gmail.com>
2015-09-21 10:29:12 +0700

    fixed bug

--- components/langs.php
+++ components/langs.php
@@ -7,7 +7,7 @@ class langs extends Component
 {
     
     public function getLang(){
-        $_l = ['th_TH' => 'thai', 'en_EN' => 'enlish', 'lo_LO' => 'lao', 'vi_VI' => 'vietnam'];
+        $_l = ['th_TH' => 'thai', 'en_EN' => 'english', 'lo_LO' => 'lao', 'vi_VI' => 'vietnam'];
         $_language = \Yii::$app->language;
         return $_l[$_language];
     }
