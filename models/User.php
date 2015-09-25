293229b43f85a314110fcae20979e58107d7be57
jatuponp <jatuponp@gmail.com>
2015-09-25 13:30:28 +0700

    add user module

--- models/User.php
+++ models/User.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\models;
 
 use Yii;
@@ -22,27 +23,27 @@ use yii\data\ActiveDataProvider;
  * @property integer $updated_at
  * @property string $password write-only password
  */
-class User extends ActiveRecord implements IdentityInterface
-{
+class User extends ActiveRecord implements IdentityInterface {
+
     const STATUS_DELETED = 0;
     const STATUS_ACTIVE = 10;
-    
+
     public $created_at;
     public $updated_at;
+    public $confirmPassword;
+    public $authType;
 
     /**
      * @inheritdoc
      */
-    public static function tableName()
-    {
+    public static function tableName() {
         return '{{%user}}';
     }
 
     /**
      * @inheritdoc
      */
-    public function behaviors()
-    {
+    public function behaviors() {
         return [
             TimestampBehavior::className(),
         ];
@@ -51,27 +52,49 @@ class User extends ActiveRecord implements IdentityInterface
     /**
      * @inheritdoc
      */
-    public function rules()
-    {
+    public function rules() {
         return [
             ['status', 'default', 'value' => self::STATUS_ACTIVE],
             ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+            [['username', 'authType'], 'required'],
+            
+            ['email', 'filter', 'filter' => 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            //['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            
+            ['password_hash', 'required'],
+            //['confirmPassword', 'compare', 'compareAttribute' => 'password_hash', 'skipOnError'=>true],
+            ['password_hash', 'string', 'min' => 6],
+            //['confirmPassword', 'safe']
         ];
     }
 
     /**
      * @inheritdoc
      */
-    public static function findIdentity($id)
-    {
+    public function attributeLabels() {
+        return [
+            'id' => 'ID',
+            'firstName' => 'ชื่อ',
+            'lastName' => 'นามสกุล',
+            'password_hash' => 'Password',
+            'confirmPassword' => 'Confirm Password',
+            'authType' => 'กำหนดสิทธิ์'
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentity($id) {
         return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
     }
 
     /**
      * @inheritdoc
      */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
+    public static function findIdentityByAccessToken($token, $type = null) {
         throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
     }
 
@@ -81,8 +104,7 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $username
      * @return static|null
      */
-    public static function findByUsername($username)
-    {
+    public static function findByUsername($username) {
         return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
     }
 
@@ -92,15 +114,14 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $token password reset token
      * @return static|null
      */
-    public static function findByPasswordResetToken($token)
-    {
+    public static function findByPasswordResetToken($token) {
         if (!static::isPasswordResetTokenValid($token)) {
             return null;
         }
 
         return static::findOne([
-            'password_reset_token' => $token,
-            'status' => self::STATUS_ACTIVE,
+                    'password_reset_token' => $token,
+                    'status' => self::STATUS_ACTIVE,
         ]);
     }
 
@@ -110,8 +131,7 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $token password reset token
      * @return boolean
      */
-    public static function isPasswordResetTokenValid($token)
-    {
+    public static function isPasswordResetTokenValid($token) {
         if (empty($token)) {
             return false;
         }
@@ -124,24 +144,21 @@ class User extends ActiveRecord implements IdentityInterface
     /**
      * @inheritdoc
      */
-    public function getId()
-    {
+    public function getId() {
         return $this->getPrimaryKey();
     }
 
     /**
      * @inheritdoc
      */
-    public function getAuthKey()
-    {
+    public function getAuthKey() {
         return $this->auth_key;
     }
 
     /**
      * @inheritdoc
      */
-    public function validateAuthKey($authKey)
-    {
+    public function validateAuthKey($authKey) {
         return $this->getAuthKey() === $authKey;
     }
 
@@ -151,8 +168,7 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $password password to validate
      * @return boolean if password provided is valid for current user
      */
-    public function validatePassword($password)
-    {
+    public function validatePassword($password) {
         return Yii::$app->security->validatePassword($password, $this->password_hash);
     }
 
@@ -161,39 +177,35 @@ class User extends ActiveRecord implements IdentityInterface
      *
      * @param string $password
      */
-    public function setPassword($password)
-    {
+    public function setPassword($password) {
         $this->password_hash = Yii::$app->security->generatePasswordHash($password);
     }
 
     /**
      * Generates "remember me" authentication key
      */
-    public function generateAuthKey()
-    {
+    public function generateAuthKey() {
         $this->auth_key = Yii::$app->security->generateRandomString();
     }
 
     /**
      * Generates new password reset token
      */
-    public function generatePasswordResetToken()
-    {
+    public function generatePasswordResetToken() {
         $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
     }
 
     /**
      * Removes password reset token
      */
-    public function removePasswordResetToken()
-    {
+    public function removePasswordResetToken() {
         $this->password_reset_token = null;
     }
-    
+
     public function getPHash($pass) {
         return Yii::$app->security->generatePasswordHash($pass);
     }
-    
+
     public function search() {
         $search = $_POST['User']['search'];
         $query = User::find();
@@ -206,4 +218,5 @@ class User extends ActiveRecord implements IdentityInterface
 
         return $dataProvider;
     }
+
 }
--- modules/admin/controllers/UserController.php
+++ modules/admin/controllers/UserController.php
@@ -27,5 +27,41 @@ class UserController extends \yii\web\Controller {
         $model = new User;
         return $this->render('index', ['model' => $model]);
     }
+    
+    public function actionUpdate($id = null) {
+        $model = new User();
+        if($model->load(\Yii::$app->request->post())){
+            $request = \Yii::$app->getRequest()->post('User');
+            $model->firstName = $request['firstName'];
+            $model->lastName = $request['lastName'];
+            $model->setPassword($request['password_hash']);
+            $model->generateAuthKey();
+            if($model->save()){
+                $r = new \yii\rbac\DbManager();
+                $role = new \yii\rbac\Role();
+                $role->name = $request['authType'];
+                $r->assign($role, $model->id);
+                $this->redirect(['user/index']);
+            }else{
+                print_r($model->getErrors());
+                exit();
+            }
+        }
+        
+        if($id){
+            $model = User::find()->where(['id'=>$id])->one();
+        }
+        
+        return $this->render('update', ['model' => $model]);        
+    }
+    
+    public function actionDelete($id) {
+        $model = User::findOne($id);        
+        if($model->delete()){
+            $r = new \yii\rbac\DbManager();
+            $r->revokeAll($model->id);
+            $this->redirect(['user/index']);
+        }
+    }
 
 }
